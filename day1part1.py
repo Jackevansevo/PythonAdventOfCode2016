b31b5c227aa6e4648c89be9ef7d1ae9a5905981d
Jack Evans <jwe@tiger-computing.co.uk>
2016-12-01 14:09:29 +0000

    Added pointless Point class...

--- day1part1.py
+++ day1part1.py
@@ -1,3 +1,18 @@
+class Point:
+    __slots__ = ['x', 'y']
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def distance_from_origin(self):
+        return(sum(map(abs, [self.x, self.y])))
+
+    def __str__(self):
+        return ('Point({}, {})'.format(self.x, self.y))
+
+
 def read_input(file):
     with open(file) as f:
         return f.read().strip().split(', ')
@@ -5,10 +20,9 @@ def read_input(file):
 
 if __name__ == '__main__':
 
-    # [TODO] Fix this abomination
-
     instructions = read_input('puzzleInputs/day1input.txt')
     directions = ('N', 'E', 'S', 'W')
+    coords = Point(0, 0)
     x, y = 0, 0
     direction = 0
 
@@ -32,15 +46,19 @@ if __name__ == '__main__':
         steps = int(''.join(steps))
 
         if directions[direction] == 'N':
+            coords.y += steps
             y += steps
 
         if directions[direction] == 'E':
+            coords.x += steps
             x += steps
 
         if directions[direction] == 'S':
+            coords.y -= steps
             y -= steps
 
         if directions[direction] == 'W':
+            coords.x -= steps
             x -= steps
 
-    print(abs(x) + abs(y))
+    print(coords.distance_from_origin)
